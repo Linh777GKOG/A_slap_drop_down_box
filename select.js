4e355b512bba9606126c05b0918c032e086c8897
Linh Vu <linh777gkog@gmail.com>
2022-07-02 20:36:14 +0700

    display style

--- select.js
+++ select.js
@@ -1,5 +1,12 @@
 export default class Select {
   constructor(element) {
     this.element = element;
+    this.options = getFormattedOptions(element.querySelectorAll('option'));
+    this.customElement = document.createElement('div');
+    this.labelElement = document.createElement('span');
+    this.optionsCustomElement = document.createElement('ul');
+    setupCustomElement(this);
+    element.style.display = 'none';
+    element.after(this.customElement);
   }
 }
